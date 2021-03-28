97c757a8a877218e095380301dfb9639ec80c6bd
Gagandeep Singh <gagan.ddun@gmail.com>
2021-03-28 17:36:23 +1100

    Add form elements

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,23 @@
+import { useState } from 'react'; 
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    return <h1>Hi</h1>;
+    const [input, setInput] = useState('');
+    const [code, setCode] = useState('');
+
+    const submitHandler = () => {
+        console.log(input);
+    }
+
+    return (
+        <div>
+            <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
+            <div>
+                <button onClick={submitHandler}>Submit</button>
+            </div>
+            <pre></pre>
+        </div>
+    );
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
