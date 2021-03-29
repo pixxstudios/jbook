ff5f1af571623e6e008aba87d315178cc2bb5584
Gagandeep Singh <gagan.ddun@gmail.com>
2021-03-29 22:07:01 +1100

    start esbuild service

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,26 @@
-import { useState } from 'react'; 
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect } from 'react'; 
 import ReactDOM from 'react-dom';
 
 const App = () => {
     const [input, setInput] = useState('');
     const [code, setCode] = useState('');
 
+    const startService = async () => {
+        const service = await esbuild.startService({
+            worker: true,
+            wasmURL: '/esbuild.wasm'
+        });
+        console.log(service);
+    }
+
     const submitHandler = () => {
         console.log(input);
     }
+
+    useEffect(() => {
+        startService();
+    }, []);
 
     return (
         <div>
